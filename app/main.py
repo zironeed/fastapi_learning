0a9845cc4db1728ed0181a25518c4dc393ae5b9b
Adil Salimgareev <forever@nzironeed.ru>
2025-03-09 16:34:24 +0500

    logging

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import time
+from uuid import uuid4
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
@@ -7,12 +8,29 @@ from fastapi.middleware.gzip import GZipMiddleware
 from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 from starlette.middleware.sessions import SessionMiddleware
 from starlette.requests import Request
+from loguru import logger
+from starlette.responses import JSONResponse
 
 from app.routers import products, category, auth, permissions, reviews
 from middleware import TimingMiddleware
 
 app = FastAPI(title='Ecommerce API v1',
               description='Educational project about an online store (ver. 1)')
+@app.middleware("http")
+async def log_middleware(request: Request, call_next):
+    log_id = str(uuid4())
+    with logger.contextualize(log_id=log_id):
+        try:
+            response = await call_next(request)
+            if response.status_code in [401, 402, 403, 404]:
+                logger.warning(f"Request to {request.url.path} failed")
+            else:
+                logger.info('Successfully accessed ' + request.url.path)
+        except Exception as ex:
+            logger.error(f"Request to {request.url.path} failed: {ex}")
+            response = JSONResponse(content={"success": False}, status_code=500)
+        return response
+logger.add("info.log", format="Log: [{extra[log_id]}:{time} - {level} - {message}]", level="INFO", enqueue=True)
 
 @app.middleware("http")
 async def modify_request_response_middleware(request: Request, call_next):
@@ -53,7 +71,12 @@ app.mount('/v1', app)
 
 @app.get('/')
 async def welcome() -> dict:
+    welcome_msg()
     return {'message': 'E-Commerce APP'}
+
+
+def welcome_msg():
+    logger.info("welcome() called!")
 
 
 @app.get("/create_session")
